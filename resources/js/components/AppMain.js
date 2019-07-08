0301f32a4ac8ed0a3f66c6432023241089392d71
Wes Dollar <wes@wesdollar.com>
2019-07-07 22:31:44 -0400

    prettier formatting

--- resources/js/components/AppMain.js
+++ resources/js/components/AppMain.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import {BrowserRouter as Router, NavLink, Route} from "react-router-dom"
-import ReactDOM from "react-dom"
-import Example from "./Example"
-import Dashboard from "./Dashboard"
-import Header from "./Header"
-import NavItem from "./NavItem"
+import { BrowserRouter as Router, NavLink, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
+import Example from "./Example";
+import Dashboard from "./Dashboard";
+import Header from "./Header";
+import NavItem from "./NavItem";
 
 /** @namespace GL.reactBase */
 /** @namespace GL.user.activeGroupTitle */
@@ -13,98 +13,104 @@ const activeGroup = GL.user.activeGroupTitle;
 const appName = GL.appName;
 
 const navItems = [
-    {
-        title: "Dashboard",
-        component: "dashboard",
-        icon: "dashboard"
-    },
-    {
-        title: "Post Round",
-        component: "example",
-        icon: "dashboard"
-    },
-    {
-        title: "Rankings",
-        component: "dashboard",
-        icon: "dashboard"
-    },
-    {
-        title: "Stats",
-        component: "example",
-        icon: "dashboard"
-    },
-    {
-        title: "Scorecards",
-        component: "dashboard",
-        icon: "dashboard"
-    },
-    {
-        title: "Post Round",
-        component: "example",
-        icon: "dashboard"
-    },
-    {
-        title: "All Golfers",
-        component: "dashboard",
-        icon: "dashboard"
-    }
+  {
+    title: "Dashboard",
+    component: "dashboard",
+    icon: "dashboard"
+  },
+  {
+    title: "Post Round",
+    component: "example",
+    icon: "dashboard"
+  },
+  {
+    title: "Rankings",
+    component: "dashboard",
+    icon: "dashboard"
+  },
+  {
+    title: "Stats",
+    component: "example",
+    icon: "dashboard"
+  },
+  {
+    title: "Scorecards",
+    component: "dashboard",
+    icon: "dashboard"
+  },
+  {
+    title: "Post Round",
+    component: "example",
+    icon: "dashboard"
+  },
+  {
+    title: "All Golfers",
+    component: "dashboard",
+    icon: "dashboard"
+  }
 ];
 
 class AppMain extends Component {
-    render() {
-        return (
-            <Router>
-                <aside id="left-panel" className="left-panel">
-                    <nav className="navbar navbar-expand-sm navbar-default">
-                        <div className="navbar-header">
-                            <button className="navbar-toggler"
-                                    type="button"
-                                    data-toggle="collapse"
-                                    data-target="#main-menu"
-                                    aria-controls="main-menu"
-                                    aria-expanded="false"
-                                    aria-label="Toggle navigation">
-                                <i className="fa fa-bars" />
-                            </button>
-                            <a className="navbar-brand" href="..">
-                                {appName}
-                            </a>
-                            <a className="navbar-brand hidden" href="..">
-                                <i className="fa fa-beer" />
-                            </a>
-                        </div>
+  render() {
+    return (
+      <Router>
+        <aside id="left-panel" className="left-panel">
+          <nav className="navbar navbar-expand-sm navbar-default">
+            <div className="navbar-header">
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#main-menu"
+                aria-controls="main-menu"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <i className="fa fa-bars" />
+              </button>
+              <a className="navbar-brand" href="..">
+                {appName}
+              </a>
+              <a className="navbar-brand hidden" href="..">
+                <i className="fa fa-beer" />
+              </a>
+            </div>
 
-                        <div id="main-menu" className="main-menu collapse navbar-collapse">
-                            <ul className="nav navbar-nav">
-                                {navItems.map((navItem, index) => (
-                                    <NavItem
-                                        title={navItem.title}
-                                        component={navItem.component}
-                                        icon={navItem.icon}
-                                        baseHref={reactBaseHref}
-                                        key={`navItem-${index}`}
-                                    />
-                                ))}
-                            </ul>
-                        </div>
-                    </nav>
-                </aside>
-                
-                <div id="right-panel" className="right-panel">
-                    <Header activeGroup={activeGroup} />
+            <div id="main-menu" className="main-menu collapse navbar-collapse">
+              <ul className="nav navbar-nav">
+                {navItems.map((navItem, index) => (
+                  <NavItem
+                    title={navItem.title}
+                    component={navItem.component}
+                    icon={navItem.icon}
+                    baseHref={reactBaseHref}
+                    key={`navItem-${index}`}
+                  />
+                ))}
+              </ul>
+            </div>
+          </nav>
+        </aside>
 
-                    <div className="content mt-30">
-                        <Route exact path={`${reactBaseHref}/`} component={Dashboard} />
-                        <Route exact path={`${reactBaseHref}/dashboard`} component={Dashboard} />
-                        <Route path={`${reactBaseHref}/example`} component={Example} />
-                    </div>
-                </div>
-            </Router>
-        );
-    }
+        <div id="right-panel" className="right-panel">
+          <Header activeGroup={activeGroup} />
+
+          <div className="content mt-30">
+            <Route exact path={`${reactBaseHref}/`} component={Dashboard} />
+            <Route
+              exact
+              path={`${reactBaseHref}/dashboard`}
+              component={Dashboard}
+            />
+            <Route path={`${reactBaseHref}/example`} component={Example} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 const target = GL.reactBase;
 if (document.getElementById(target)) {
-    ReactDOM.render(<AppMain />, document.getElementById(target));
+  ReactDOM.render(<AppMain />, document.getElementById(target));
 }
--- resources/js/components/NavItem.jsx
+++ resources/js/components/NavItem.jsx
@@ -1,28 +1,27 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
-import {NavLink} from "react-router-dom"
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 
 class NavItem extends Component {
+  render() {
+    const { title, component, icon, baseHref } = this.props;
 
-    render() {
-        const {title, component, icon, baseHref} = this.props;
-
-        return (
-            <li>
-                <NavLink to={`${baseHref}/${component}`}>
-                    <i className={`menu-icon fa fa-${icon}`} />
-                    {title}
-                </NavLink>
-            </li>
-        )
-    }
+    return (
+      <li>
+        <NavLink to={`${baseHref}/${component}`}>
+          <i className={`menu-icon fa fa-${icon}`} />
+          {title}
+        </NavLink>
+      </li>
+    );
+  }
 }
 
 NavItem.propTypes = {
-    title: PropTypes.string.isRequired,
-    component: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-    baseHref: PropTypes.string.isRequired
-}
+  title: PropTypes.string.isRequired,
+  component: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  baseHref: PropTypes.string.isRequired
+};
 
-export default NavItem
+export default NavItem;
